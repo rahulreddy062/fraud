f8fb73de920a3a854db03afe5005894eeb76c142
Ankit Prakash <ankit.prakash687@gmail.com>
2020-03-04 09:51:11 -0500

    Update insight layout

--- src/Components/Insights/Insights.js
+++ src/Components/Insights/Insights.js
@@ -7,48 +7,45 @@ import PieChart from '../../Layout/PieChart/PieChart';
 import FraudInsights from '../../Layout/Table/Table';
 import PatternChart from '../../Layout/PatternChart/PatternChart';
 
-
 const useStyles = makeStyles(theme => ({
   root: {
-    flexGrow: 1,
+    flexGrow: 1
   },
   paper: {
     padding: theme.spacing(2),
     textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
+    color: theme.palette.text.secondary
+  }
 }));
 
 export default function Insights() {
-  
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
-        <br/>
-        <br/>
+      <br />
+      <br />
       <Grid container spacing={3}>
-      <Grid item xs={12} md={8}>
-  <Card className={classes.paper}>
-      <LineChart style = {{boxShadow:"3px 3px 3px #9E9E9E"}}/>
-      </Card>
+        <Grid item xs={12} sm={6}>
+          <Card className={classes.paper}>
+            <LineChart style={{ boxShadow: '3px 3px 3px #9E9E9E' }} />
+          </Card>
         </Grid>
-          <Grid item xs={12} md={8}>
-            <Card style={{paddingBottom:"4px",paddingTop:"28px"}}>
-              <PieChart />
-              </Card>
+        <Grid item xs={12} sm={6}>
+          <Card style={{ paddingBottom: '4px', paddingTop: '28px' }}>
+            <PieChart />
+          </Card>
+        </Grid>
+
+        <br />
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={6}>
+            <PatternChart />
           </Grid>
-      </Grid>
-      <br/>
-      <Grid container spacing={3}>
-      <Grid item xs={12} md={4}>
-        <PatternChart/>
-      </Grid>
-      <Grid item xs={12} md={4}>
-      
-        <FraudInsights style = {{boxShadow:"3px 3px 3px #9E9E9E"}}/>
-        
-      </Grid>
+          <Grid item xs={12} sm={6}>
+            <FraudInsights style={{ boxShadow: '3px 3px 3px #9E9E9E' }} />
+          </Grid>
+        </Grid>
       </Grid>
     </div>
   );
